045d374e0777a5f98255bc27446f30d385d083c5
Marvin Chomba <marvin.chomba24@gmail.com>
2018-09-14 12:51:54 +0300

    Fix the bug on the triangle function

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 function triangle() {
-  var a = document.getElementById("num1").value;
-  var b = document.getElementById("num2").value;
-  var c = document.getElementById("num3").value;
+  var a = parseInt(document.getElementById("num1").value);
+  var b = parseInt(document.getElementById("num2").value);
+  var c = parseInt(document.getElementById("num3").value);
+  
   var display = document.getElementById("display");
-  if(a + b > c && b + c > a && c + a > b) {
-    if(a === b && b === c && c === a) {
-      display.textContent = "Equilateral";
-    } else if(a === b || a === c || b === c) {
-      display.textContent = "Isosceles";
-    } else {
-      display.textContent === "Scalene";
-    }
-  } else if (isNaN(a) || isNaN(b) || isNaN(c)) {
-    display.textContent = "Invalid"
-  } else {
-    display.textContent = "Not a triangle";
-  }
+	if(a + b > c && b + c > a && c + a > b) {
+		if(a === b && a === c) {
+			display.textContent = "Equilateral";
+		} else if(a === b || b === c || a === c) {
+			display.textContent = "Isosceles";
+		} else {
+			display.textContent = "Scalene";
+		}
+	} else if(isNaN(a) || isNaN(b) || isNaN(c)) {
+		display.textContent = "Invalid";
+	} else {
+		display.textContent = "Not a triangle";
+	}
 }
