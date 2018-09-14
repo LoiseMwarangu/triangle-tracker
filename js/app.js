3770d827a12ecc5fae11e5424609b199458a4577
Marvin Chomba <marvin.chomba24@gmail.com>
2018-09-14 15:19:04 +0300

    Add the fonts to the body and the header

--- js/app.js
+++ js/app.js
@@ -8,14 +8,14 @@ function triangle() {
 function track(a,b,c) {
   if(a + b > c && b + c > a && c + a > b) {
     if(a === b && a === c) {
-      display.innerHTML = "<p class='display-text'>This is an <span class='type'>Equilateral</span> Triangle"
+      display.innerHTML = "<p class='display-text'>That is an <span class='type'>Equilateral</span> Triangle"
     } else if(a === b || b === c || a === c) {
-      display.innerHTML = "<p class='display-text'>This is an <span class='type'>Isosceles</span> Triangle"
+      display.innerHTML = "<p class='display-text'>That is an <span class='type'>Isosceles</span> Triangle"
     } else {
-      display.innerHTML = "<p class='display-text'>This is a <span class='type'>Scalene</span> Triangle"
+      display.innerHTML = "<p class='display-text'>That is a <span class='type'>Scalene</span> Triangle"
     } 
   }else {
-    display.innerHTML = "<p class='display-text'>This is <span class='not'>NOT</span> a triangle"
+    display.innerHTML = "<p class='display-text'>That is <span class='not'>NOT</span> a triangle"
   }
 }
 
